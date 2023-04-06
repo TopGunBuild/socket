8b5cf705c82ee5ca5ac1de5b1c9d15e7e743bc13
kiborg <easysolpro@gmail.com>
2023-04-06 20:49:10 +0300

    chore: Change release.config.js

--- release.config.js
+++ release.config.js
@@ -28,7 +28,7 @@ module.exports = {
     [
       "@semantic-release/git",
       {
-        assets: ["CHANGELOG.md", "package.json", "pnpm-lock.yaml", "yarn.lock"],
+        assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
         message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
