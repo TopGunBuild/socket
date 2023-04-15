3e4be29c847c7917646f2cd6544819092e71127a
kiborg <easysolpro@gmail.com>
2023-04-15 19:26:53 +0300

    fix: add script for creating packages

---
+++ tools/prepare-package.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const PACKAGE_DIRS = ['channel'];
+
+const getContent = function (name)
+{
+    return `{
+    "name": "topgun-socket/${name}",
+    "typings": "../dist/${name}.d.ts",
+    "main": "../dist/${name}.js",
+    "module": "../dist/${name}.mjs",
+    "sideEffects": false
+}`;
+};
+
+for (const dir of PACKAGE_DIRS)
+{
+    if (!fs.existsSync(dir))
+    {
+        fs.mkdirSync(dir);
+        fs.writeFileSync(path.join(__dirname, '..', dir, 'package.json'), getContent(dir));
+    }
+}
