516cc124248ef928ef0b8f30a97dcd3af33276f5
kiborg <easysolpro@gmail.com>
2023-05-01 16:42:18 +0300

    feat(socket-server): remove `http` param from `listen`. add typings to `attach`

--- src/socket-server/index.ts
+++ src/socket-server/index.ts
@@ -1,27 +1,29 @@
+import { Server } from 'http';
 import { TGServerSocketGateway } from './server';
 import { TGServerSocketGatewayOptions } from './types';
+import { isNode } from '../utils/is-node';
 
 export * from './action';
 export * from './server';
 export * from './server-socket';
 export * from './types';
 
+
+type ListenFn = (...rest: any[]) => void;
+
 /**
  * Captures upgrade requests for a http.Server.
  */
-export function attach(server: any, options?: TGServerSocketGatewayOptions)
+export function attach(server: Server, options?: TGServerSocketGatewayOptions)
 {
     options            = options || {};
     options.httpServer = server;
     return new TGServerSocketGateway(options);
 }
 
-type ListenFn = (...rest: any[]) => void;
-
 export function listen(
-    http: any,
     port: number,
-    options?: TGServerSocketGatewayOptions | ListenFn,
+    options?: TGServerSocketGatewayOptions|ListenFn,
     fn?: ListenFn
 ): TGServerSocketGateway
 {
@@ -31,15 +33,23 @@ export function listen(
         options = {};
     }
 
-    const server = http.createServer((req, res) =>
+    if (isNode())
     {
-        res.writeHead(501);
-        res.end('Not Implemented');
-    });
+        const http   = require('http');
+        const server = http.createServer((req, res) =>
+        {
+            res.writeHead(501);
+            res.end('Not Implemented');
+        });
 
-    const gateway        = attach(server, options);
-    gateway.httpServer = server;
-    server.listen(port, fn);
+        options.httpServer = server;
+        const gateway      = attach(server, options);
+        server.listen(port, fn);
 
-    return gateway;
+        return gateway;
+    }
+    else
+    {
+        return new TGServerSocketGateway(options);
+    }
 }
